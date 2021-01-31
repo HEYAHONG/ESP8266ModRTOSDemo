1ec27fcd3e55ae7156c2b0c20df2baae678dd06b
HEYAHONG <2229388563@qq.com>
2021-01-31 11:54:40 +0800

    添加不使用CMake直接使用make编译所需配置

--- main/component.mk
+++ main/component.mk
@@ -2,3 +2,11 @@
 # "main" pseudo-component makefile.
 #
 # (Uses default behaviour of compiling all source files in directory, adding 'include' to include path.)
+COMPONENT_ADD_INCLUDEDIRS := include
+
+COMPONENT_OBJS = main.o  src/init.o  src/wifi_station.o src/appcmd.o src/mqttapp.o  
+
+COMPONENT_SRCDIRS := src ./
+
+COMPONENT_SUBMODULES += main
+
