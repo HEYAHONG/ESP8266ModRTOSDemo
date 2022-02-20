fc8cac606f4afc0c4439f9df36d8df06c8eb0b62
HEYAHONG <2229388563@qq.com>
2022-02-20 16:55:19 +0800

    修改目录结构

--- main/component.mk
+++ main/component.mk
@@ -2,15 +2,8 @@
 # "main" pseudo-component makefile.
 #
 # (Uses default behaviour of compiling all source files in directory, adding 'include' to include path.)
-COMPONENT_ADD_INCLUDEDIRS := include
-
-COMPONENT_OBJS = main.o  src/init.o  src/wifi_station.o src/appcmd.o src/mqttapp.o src/tftpd.o
-
-
-#默认RTOS框架中为启用tftp,在此处启用
-COMPONENT_OBJS += tftp/tftp_server.o
-
-COMPONENT_SRCDIRS := src ./
+COMPONENT_ADD_INCLUDEDIRS := ./
+COMPONENT_SRCDIRS :=  ./
 
 #默认RTOS框架中为启用tftp,在此处启用
 COMPONENT_SRCDIRS += tftp
